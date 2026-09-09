f54dc4560c17bba07d7d87807f1c6a1a8ff276f2
bogbuche <blessingogbuche.com>
2026-09-09 17:03:53 +0100

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
+import calendar
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    year = 2026
+    month = 9
+
+    month_name = calendar.month_name[month]
+    month_days = calendar.monthcalendar(year, month)
+
+    return render_template(
+        "index.html",
+        year=year,
+        month_name=month_name,
+        month_days=month_days
+    )
+
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-
